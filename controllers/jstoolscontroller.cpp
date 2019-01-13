ed6500511682745ff1e9632125484aea978faab7
hybtalented <heyb95@chinaunicom.cn>
2019-01-13 22:51:43 +0800

    add jstool controll, add jstool/base64transfer view

--- controllers/jstoolscontroller.cpp
+++ controllers/jstoolscontroller.cpp
@@ -9,20 +9,17 @@ void JstoolsController::base64transfer()
 	case Tf::Get:
 		render();
 		break;
-	case Tf::Post:			
-		if (httpRequest().hasJson()) {
-			auto obj = httpRequest().jsonData();
-			auto requesttype = obj["type"].toString();
-			if (requesttype != "") {
-				if (requesttype == "download") {
-					auto base64 = obj["base64"].toString();
-					qDebug() << base64;
-					auto filename = obj["filename"].toString();
-					auto fileobj = QByteArray::fromBase64(base64.toLocal8Bit(), QByteArray::Base64Option::Base64Encoding);
-					response.header().setContentType("multipart/form-data");
-					response.header().setRawHeader("Content-Disposition", filename.toUtf8());
-					response.setBody(fileobj);
-				}
+	case Tf::Post:	
+		auto obj = httpRequest().formItems("jstools");
+		auto requesttype = obj["type"].toString();
+		if (requesttype != "") {
+			if (requesttype == "download") {
+				auto base64 = obj["base64"].toString();
+				auto filename = obj["filename"].toString();
+				auto fileobj = QByteArray::fromBase64(base64.toLocal8Bit(), QByteArray::Base64Option::Base64Encoding);
+				response.header().setContentType("multipart/form-data");
+				response.header().setRawHeader("Content-Disposition", filename.toUtf8());
+				response.setBody(fileobj);
 			}
 		}
 		break;
