451abc7e54b8108fc43fc530891c2e1b9c2409d9
hybtalented <heyb95@chinaunicom.cn>
2019-01-17 15:39:32 +0800

    add json compress and format function to jstools/base64transfer view

--- controllers/jstoolscontroller.cpp
+++ controllers/jstoolscontroller.cpp
@@ -21,6 +21,18 @@ void JstoolsController::base64transfer()
 				response.header().setRawHeader("Content-Disposition", QString(u8"attachment; filename=\"%1\"").arg(filename).toUtf8());
 				response.setBody(fileobj);
 			}
+			else if (requesttype == "jsonformat") {
+				auto doc = obj["jsontext"].toString();
+				QJsonDocument jsondoc = QJsonDocument::fromJson(doc.toUtf8());
+				response.header().setContentType(u8"application/text");
+				response.setBody(jsondoc.toJson(QJsonDocument::Indented));
+			}
+			else if (requesttype == "jsoncompress") {
+				auto doc = obj["jsontext"].toString();
+				QJsonDocument jsondoc = QJsonDocument::fromJson(doc.toUtf8());
+				response.header().setContentType(u8"application/text");
+				response.setBody(jsondoc.toJson(QJsonDocument::Compact));
+			}
 		}
 		break;
 	}
